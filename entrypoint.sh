fbbb36032c65f05ca86e648f6fd290abdba4142b
Francisco Molina <fmolinaleiv+github@gmail.com>
2024-11-06 20:52:32 -0300

    FM: more entrypoint shenanigans

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update -y && apt-get upgrade -y
-apt-get install libx11-6 libgl1 g++ -y
+apt-get install -y libx11-6 libgl1 g++
 yes | pip install -U pip
 yes | pip install -r requirements.txt
 pip install torch-scatter -f https://data.pyg.org/whl/torch-2.4.1+cu118.html
