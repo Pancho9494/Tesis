975134b744e804de275bb762b79ba42598aa46dc
Francisco Molina <fmolinaleiv+github@gmail.com>
2024-11-06 20:50:59 -0300

    FM: git typo on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update -y && apt-get update -y
+apt-get update -y && apt-get upgrade -y
 apt-get install libx11-6 libgl1 g++ -y
 yes | pip install -U pip
 yes | pip install -r requirements.txt
