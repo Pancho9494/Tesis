b4e71c395cb373d44bdce664b95028af0987ef84
Francisco Molina <fmolinaleiv+github@gmail.com>
2025-06-27 19:10:41 -0400

    rebuilt .sif

--- scripts/build_sif.sh
+++ scripts/build_sif.sh
@@ -3,25 +3,25 @@
 FORCE_DOCKER_REBUILD=false
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-    --force-docker-rebuild)
-      FORCE_DOCKER_REBUILD=true
-      ;;
-    *)
-      echo "Unknown option: $1"
-      echo "Usage: $0 [--force-docker-rebuild]"
-      exit 1
-      ;;
+  --force-docker-rebuild)
+    FORCE_DOCKER_REBUILD=true
+    ;;
+  *)
+    echo "Unknown option: $1"
+    echo "Usage: $0 [--force-docker-rebuild]"
+    exit 1
+    ;;
   esac
   shift
 done
 
 # Does the docker image already exist?
-if $FORCE_DOCKER_REBUILD || [ -z "$(docker images -q lim/cuda12.4-cudnn9:latest 2>/dev/null)" ]; then
-  echo "Docker image lim/cuda12.4-cudnn9:latest not found, building from scratch..."
+if $FORCE_DOCKER_REBUILD || [ -z "$(docker images -q lim/cuda12.4-cudnn9:leftraru 2>/dev/null)" ]; then
+  echo "Docker image lim/cuda12.4-cudnn9:leftraru not found, building from scratch..."
   docker build \
     --build-arg TORCH_CUDA_ARCH_LIST="$(nvidia-smi --query-gpu=compute_cap --format=csv,noheader | tr '\n' ';' | sed 's/;$//')+PTX" \
-    -f containers/docker/Dockerfile \
-    -t lim/cuda12.4-cudnn9:latest .
+    -f containers/docker/Dockerfile.trainer \
+    -t lim/cuda12.4-cudnn9:leftraru .
 fi
 
-apptainer build --fakeroot containers/apptainer/iae.sif containers/apptainer/iae.def
+apptainer build --fakeroot containers/apptainer/trainer.sif containers/apptainer/trainer.def
